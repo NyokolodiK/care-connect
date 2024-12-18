21e7f41c37d0c80bb07abc0f7433b291142dea84
Kagiso <kagiso@bazaruto.com>
2024-12-18 06:27:15 +0200

    Create home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,33 @@
-import { Button } from "@/components/ui/button";
+import PatientForm from "@/components/forms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-      <h1 className="text-white text-3xl underline">Home</h1>
-      <Button>Test</Button></>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="logo"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">© {new Date().getFullYear()} Care Connect All rights reserved</p>
+            <Link href="/?admin=true" className="text-green-500">Admin</Link>
+          </div>
+        </div>
+      </section>
+
+      <Image src="/assets/images/onboarding-img.png"
+        alt="onboarding"
+        width={1000}
+        height={1000}
+        className="side-img max-w-[50%]" />
+    </div>
   )
 }
