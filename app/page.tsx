bfb7a6463cd17a5e863c4606d69700691870a9a4
Kagiso <kagiso@bazaruto.com>
2024-12-18 06:06:37 +0200

    App cleanup and setup basic style

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <>Home</>
+    <>
+      <h1 className="text-white text-3xl underline">Home</h1>
+      <Button>Test</Button></>
+  )
+}
